ReallySnow/twrp_device_google_taimen

taimen:lineage -> omni and add prebuilt kernel

AndroidProducts.mk
@@ -15,5 +15,6 @@
 #
 
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_taimen.mk \
     $(LOCAL_DIR)/aosp_taimen.mk \
-    $(LOCAL_DIR)/lineage_taimen.mk \
+

BoardConfig.mk
@@ -15,7 +15,8 @@
 #
 
 # Allow Lineage config to override others
--include device/google/taimen/BoardConfigLineage.mk
+TARGET_PREBUILT_KERNEL := device/google/taimen/prebuilt/Image.gz-dtb
+TARGET_KERNEL_CONFIG := lineageos_taimen_defconfig
 
 TARGET_BOOTLOADER_BOARD_NAME := taimen
 DEFAULT_LOW_PERSISTENCE_MODE_BRIGHTNESS := 0x0000008c

kyoto44/android_device_google_muskie

AndroidProducts.mk
@@ -15,6 +15,7 @@
 #
 
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_walleye.mk \
     $(LOCAL_DIR)/aosp_walleye.mk \
     $(LOCAL_DIR)/aosp_walleye_test.mk \
     $(LOCAL_DIR)/aosp_walleye_hwasan.mk \

walleye/BoardConfig.mk
@@ -19,6 +19,8 @@ DEFAULT_LOW_PERSISTENCE_MODE_BRIGHTNESS := 0x00000056
 
 include device/google/wahoo/BoardConfig.mk
 -include vendor/google_devices/muskie/proprietary/BoardConfigVendor.mk
+TARGET_PREBUILT_KERNEL := device/google/walleye/prebuilt/Image.gz-dtb
+TARGET_KERNEL_CONFIG := lineageos_walleye_defconfig
 
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
 
@@ -40,5 +42,3 @@ TARGET_RECOVERY_UI_LIB := \
     librecovery_ui_walleye \
     libfstab
 
-# Allow Lineage config to override others
--include device/google/muskie/walleye/BoardConfigLineage.mk